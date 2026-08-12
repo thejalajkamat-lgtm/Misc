6ba0b32aa2c12febc26fa22808e4e790d6c81cb9
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-12 22:23:27 +0530

    added app.js

---
+++ frontend/app.js
@@ -0,0 +1,24 @@
+const stu1 = {
+    name: "jalaj kamat",
+    age: 25,
+    marks: 95,
+    getMarks: function(){
+        return this.marks;
+    },
+};
+const stu2 = {
+    name: "niraj kamat",
+    age: 45,
+    marks: 25,
+    getMarks: function(){
+        return this.marks;
+    },
+};
+const stu3 = {
+    name: "nitin kamat",
+    age: 65,
+    marks: 65,
+    getMarks: function(){
+        return this.marks;
+    },
+};
