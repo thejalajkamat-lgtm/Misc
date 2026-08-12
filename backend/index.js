7ffb07a3f4d2978d0076420d4b662c5fdf2dc21b
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-12 21:51:28 +0530

    hrh

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
+// const cors = require("cors");
 const port = 8080;
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.get("/register", (req, res) => {
     let {user, password} = req.query;
@@ -9,8 +11,9 @@ app.get("/register", (req, res) => {
     res.send(`You got a GET response. Congratulations ${user}`)
 });
 app.post("/register", (req, res) => {
-    let {user, password} = req.query;
-    console.log("standard POST response")
+    let {user, password} = req.body;
+    console.log(req.body)
+    // console.log("Standard Post request")
     res.send(`You got a POST response. Congratulations ${user}`)
 });
 app.listen(port, () => {
